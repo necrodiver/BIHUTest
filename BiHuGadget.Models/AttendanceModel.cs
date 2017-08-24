a33652f466e9f592da22a151e41c8eca99f5df87
necrodiver <neclodiver@live.com>
2017-08-24 17:35:46 +0800

    添加新的打卡Model

---
+++ BiHuGadget.Models/AttendanceModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiHuGadget.Models
+{
+    /// <summary>
+    /// 考勤表，只能导入，不可手动修改
+    /// </summary>
+    public class AttendanceModel
+    {
+        public int AttendanceId { get; set; }
+        public int UserId { get; set; }
+        /// <summary>
+        /// 打卡时间
+        /// </summary>
+        public DateTime? ClockTime { get; set; }
+        /// <summary>
+        /// 打卡状态
+        /// </summary>
+        public int ClockStatus { get; set; }
+    }
+}
---
+++ BiHuGadget.Models/ClockMark.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiHuGadget.Models
+{
+    /// <summary>
+    /// 打卡标记(可改动)
+    /// </summary>
+    public class ClockMark
+    {
+        public int ClockMarkId { get; set; }
+        /// <summary>
+        /// 标记记录说明
+        /// </summary>
+        public string Record { get; set; }
+        /// <summary>
+        /// 标记状态
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// 是否审核通过，审核通过：1，未通过：0
+        /// </summary>
+        public int Pass { get; set; }
+    }
+}
